2eec28c6098dee84941b1b2387d5b27a1354aeb9
David Newton <newtondavi2@dahra.ecs.vuw.ac.nz>
2016-03-25 00:13:29 +1300

    Search Function complete

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+	console.log("error1");
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
@@ -49,6 +50,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+	console.log("error2");
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
--- routes/index.js
+++ routes/index.js
@@ -3,29 +3,33 @@ var router = express.Router();
 var basex = require('basex');
 var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
 client.execute("OPEN Colenso");
+var tei = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; ";
 
-console.log("WORK");
-console.log("fuck");
+//Get the Homepage
+router.get("/",function(req,res){
+	client.execute(tei + " (//name[@type='place'])[1] ", function (error, result) {
+			if(error){console.error(error);}
+			else {
+				res.render('index', { title: 'Colenso Project', place: result.result, database_list: [] });
+			}
+		}
+	);
+});
 
-//router.get("/", function(req, res) {
- // res.render('index', { title: 'Colenso' });
-//});
-/* GET home page. */
-router.get('/', function(req, res){
-	client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" + " (//name[@type='place'])[1] ",
-	function(error, result){
-		if(error){ 
-			console.error(error);}
-		else {
-  			res.render('index', { title: 'Colenso' });
- 		}
-  		}
- 		);
+//Search 
+router.get("/search",function(req,res){
+	console.log("tei + searchString: " + tei + req.query.searchString);
+	client.execute(tei+ req.query.searchString,
+		function (error, result) {
+			if(error){ 
+				console.error(error);
+			}
+			else {
+				console.log("result: " + result.result);
+				res.render('index', { title: 'Colenso Project', search_result: result.result });
+			}
+		}
+	);
 });
 
 module.exports = router;
-
-router.get('/search', function(req,res){
-	console.log("FGUDIFHI");
-	res.render('search', { title: 'Colenso', content: req.query.searchString });
-});
